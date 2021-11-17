6c0cc16b6cdec15dde3594d8506c0a400703c2ef
Thomas Dejardins <tdejardins@gmail.com>
2021-11-17 18:45:38 +0100

    feat(modal): add function close modal #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,14 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+/////////// 1 - TODO : close modal ///////////
+
+const closeBtn = document.querySelector(".close");
+
+closeBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
